2fc6ad6241db8fa4c4be2e57ca3183a958b76444
yoshihisa okada <yookada@winoo.co.jp>
2020-07-30 22:31:49 +0900

    applied checkpoint

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -11,54 +11,52 @@ class Fib extends Component {
   componentDidMount() {
     this.fetchValues();
     this.fetchIndexes();
-  };
+  }
 
-  async fetchValues () {
+  async fetchValues() {
     const values = await axios.get('/api/values/current');
     this.setState({ values: values.data });
-  };
+  }
 
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
-    this.setState({ seenIndexes: seenIndexes.data });
-  };
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
+  }
 
-  handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
 
     await axios.post('/api/values', {
       index: this.state.index
     });
-    this.setState({ index : '' });
-
-  }
+    this.setState({ index: '' });
+  };
 
   renderSeenIndexes() {
-    // seenIndexes is an array, which has the property "number". join(', ') let all the numbers pulled out to be joined together with conmas
-    return this.state.seenIndexes.map(({ number }) => number).join(', ')
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
     const entries = [];
 
-    for(let key in this.state.values) {
+    for (let key in this.state.values) {
       entries.push(
         <div key={key}>
           For index {key} I calculated {this.state.values[key]}
         </div>
-      )
+      );
     }
 
-    return entries
+    return entries;
   }
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Enter your index:
-          </label>
+          <label>Enter your index:</label>
           <input
             value={this.state.index}
             onChange={event => this.setState({ index: event.target.value })}
@@ -66,18 +64,14 @@ class Fib extends Component {
           <button>Submit</button>
         </form>
 
-        <h3>
-          Indexes I have seen:
-        </h3>
+        <h3>Indexes I have seen:</h3>
         {this.renderSeenIndexes()}
-        <h3>
-          Calculated Values:
-        </h3>
+
+        <h3>Calculated Values:</h3>
         {this.renderValues()}
       </div>
-    )
+    );
   }
-
-};
+}
 
 export default Fib;
